f7a9a6bc0cdf20c09aa064db6cce2b3c1a9120d5
Tim Roush <troush@mail.greenriver.edu>
2017-05-13 16:16:38 -0500

    Completed #2

--- intro.js
+++ intro.js
@@ -12,3 +12,26 @@ intro();
 function intro() {
   console.log("Introduction to JavaScript");
 }
+
+
+// Print 1 to 100 to the console replacing numbers divisible by 3 with "Fizz"
+// and numbers divisible by 5 with "Buzz".
+
+function fizzBuzz1() {
+  for (var i = 1; i <= 100; i++) {
+    if (i % 3 === 0) console.log("Fizz");
+    else if (i % 5 === 0) console.log("Buzz");
+    else console.log(i);
+  }
+}
+
+
+// Print 1 to 100 to the console replacing numbers divisible by both 3 and 5
+// with "FizzBuzz".
+
+function fizzBuzz2() {
+  for (var i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz");
+    else console.log(i);
+  }
+}
